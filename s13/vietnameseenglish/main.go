0254602898eb7090c46e83b2cd98124fa5171f32
Chris Nguyen <chris@tnwebdev.com>
2020-10-22 19:18:43 -0700

    checking in s13 code example and exercies

--- s13/vietnameseenglish/main.go
+++ s13/vietnameseenglish/main.go
@@ -1,4 +1,4 @@
-// vietnameseenglish - creat Vietnamese to English dictionary program using existing englishvietnames
+// vietnameseenglish - create Vietnamese to English dictionary program using existing englishvietnames
 package main
 
 import (
@@ -25,13 +25,13 @@ func main() {
 	// we can add or replace existing word
 	dict["up"] = "lên"
 	dict["down"] = "xuống"
-	dict["tasty"] = "ngon" // we can fix the wrong word by over write it
+	dict["tasty"] = "ngon" // we can fix the wrong word by overwrite it
 	dict["empty"] = ""
 
-	// create a new map for vietnamese dictionary
+	// create a new map for vietnamese to english dictionary
 	vietnamese := make(map[string]string, len(dict)) // len(dict) is not a required feature.
 
-	// now clone the keys and values pair from the dict by reverse the value to key and save
+	// now clone the key and value pair from the dict by reverse the value to key and save
 	// to vietnamese dictionary
 	for k, v := range dict {
 		vietnamese[v] = k
