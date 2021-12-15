21630f47bbca0b38adb7b948dc70d25d63a2fa84
Thanh Nguyen <chrisng9@outlook.com>
2021-12-15 12:00:28 -0800

    Addming sec1 example code

--- sec1/variable/main.go
+++ sec1/variable/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/tnfullstack/gobc01/sec1/variable/doctor"
 )
@@ -18,14 +19,20 @@ func main() {
 
 	for {
 		fmt.Print("-> ")
+
 		userInput, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Can not read user input")
 			return
 		}
+		userInput = strings.ToLower(userInput)
 
-		test := doctor.Response(userInput)
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
 
-		fmt.Println(test)
+		if userInput == "q" || userInput == "quit" || userInput == "exit" {
+			break
+		}
+		fmt.Println(doctor.Response(userInput))
 	}
 }
