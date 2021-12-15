e74e61d29228ebaa39e25e60583c001abaf8fc6c
Thanh Nguyen <chrisng9@outlook.com>
2021-12-15 11:18:59 -0800

    Adding sec1 code example

--- sec1/variable/main.go
+++ sec1/variable/main.go
@@ -1,19 +1,31 @@
-// Variable
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/tnfullstack/gobc01/sec1/variable/doctor"
+)
 
 func main() {
-	var (
-		str  = "Hello World!"
-		str1 = "Go is the most fun and easy to learn language"
-	)
 
-	fmt.Println(str)
+	reader := bufio.NewReader(os.Stdin)
 
-	saySomeThing(str1)
-}
+	whatToSay := doctor.Intro()
 
-func saySomeThing(s string) {
-	fmt.Println(s)
+	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Can not read user input")
+			return
+		}
+
+		test := doctor.Response(userInput)
+
+		fmt.Println(test)
+	}
 }
