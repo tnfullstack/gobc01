7b85741c6dba77f6b3a79fa05dee40826391622a
Thanh Nguyen <chrisng9@outlook.com>
2021-12-16 15:46:27 -0800

    Adding sec3 example code

---
+++ sec3/hammerbitcoin/main.go
@@ -0,0 +1,20 @@
+// Hammer bitcoin game
+package main
+
+import (
+	"fmt"
+
+	"github.com/tnfullstack/gobc01/sec3/hammerbitcoin/game"
+)
+
+func main() {
+	playAgain := true
+
+	for playAgain {
+		// game.Play()
+		playAgain = game.GetYesOrNo("Would you like to play again (y/n)?")
+	}
+
+	fmt.Println("")
+	fmt.Println("Goodbye.")
+}
