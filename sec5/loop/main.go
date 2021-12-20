d80d21e8a20f5da8a2924ab995f12aead71eeec7
Thanh Nguyen <chrisng9@outlook.com>
2021-12-19 22:33:04 -0800

    Adding loop example

---
+++ sec5/loop/main.go
@@ -0,0 +1,17 @@
+// For loop
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a variable num and assing interger 100
+	num := 10
+	sum := 0
+
+	// Adding 1 to 100 and print the all values using for loop
+	for i := 1; i <= num; i++ {
+		fmt.Printf("#%d + %d = ", i, sum)
+		sum += i
+		fmt.Printf("%d\n", sum)
+	}
+}
