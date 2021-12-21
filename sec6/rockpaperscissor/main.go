59dd65704ecf67eeb161973cf1a3e8513a8eb82e
Thanh Nguyen <chrisng9@outlook.com>
2021-12-20 20:42:06 -0800

    Adding rock-paper-scissors game

--- sec6/rockpaperscissor/main.go
+++ sec6/rockpaperscissor/main.go
@@ -19,70 +19,88 @@ const (
 )
 
 func main() {
+
 	rand.Seed(time.Now().UnixNano())
 	playerChoice := ""
 	playerValue := -1
 
 	computerValue := rand.Intn(3)
 
-	reader := bufio.NewReader(os.Stdin)
-
+	// Clear the screen each time the game start
 	clearScreen()
 
-	fmt.Print("Please enter rock, paper, or scissors -> ")
-	playerChoice, _ = reader.ReadString('\n')
-	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+	// Set the number of time the game repeat play
+	round := 3
 
-	playerChoice = strings.ToLower(playerChoice)
+	// The for loop allow the game play upto the number of round defined above
+	for round >= 0 {
+		// Read keyboard input from command-line terminal
+		reader := bufio.NewReader(os.Stdin)
 
-	// if and else if statements
-	if playerChoice == "rock" {
-		playerValue = ROCK
-	} else if playerChoice == "paper" {
-		playerValue = PAPER
-	} else if playerChoice == "scissors" {
-		playerValue = SCISSORS
-	}
+		fmt.Print("Please enter rock, paper, or scissors -> ")
+		playerChoice, _ = reader.ReadString('\n')
+		playerChoice = strings.Replace(playerChoice, "\n", "", -1)
 
-	// switch statements
-	switch computerValue {
-	case ROCK:
-		fmt.Println("Computer chose ROCK")
-	case PAPER:
-		fmt.Println("Computer chose PAPPER")
-	case SCISSORS:
-		fmt.Println("Computer chose SCISSORS")
-	default:
-	}
+		playerChoice = strings.ToLower(playerChoice)
 
-	fmt.Println()
+		// if and else if statements
+		if playerChoice == "rock" {
+			playerValue = ROCK
+		} else if playerChoice == "paper" {
+			playerValue = PAPER
+		} else if playerChoice == "scissors" {
+			playerValue = SCISSORS
+		}
 
-	if playerValue == computerValue {
-		fmt.Println("It's a tide.")
-	} else {
-		switch playerValue {
+		// switch statements practice
+		switch computerValue {
 		case ROCK:
-			if computerValue == PAPER {
-				fmt.Println("Computer wins.")
-			} else {
-				fmt.Println("Player wins.")
-			}
+			fmt.Println("Computer chose ROCK")
 		case PAPER:
-			if computerValue == SCISSORS {
-				fmt.Println("Computer wins.")
-			} else {
-				fmt.Println("Player wins.")
-			}
+			fmt.Println("Computer chose PAPPER")
 		case SCISSORS:
-			if computerValue == ROCK {
-				fmt.Println("Computer wins.")
-			} else {
-				fmt.Println("Player wins.")
-			}
+			fmt.Println("Computer chose SCISSORS")
 		default:
 		}
+
+		// Print out the user input choise
+		fmt.Println("Player choice", strings.ToUpper(playerChoice))
+		fmt.Println()
+
+		// Decide the winner
+		if playerValue == computerValue {
+			fmt.Println("It's a tide.")
+		} else {
+			switch playerValue {
+			case ROCK:
+				if computerValue == PAPER {
+					fmt.Println("Computer wins.")
+				} else {
+					fmt.Println("Player wins.")
+				}
+			case PAPER:
+				if computerValue == SCISSORS {
+					fmt.Println("Computer wins.")
+				} else {
+					fmt.Println("Player wins.")
+				}
+			case SCISSORS:
+				if computerValue == ROCK {
+					fmt.Println("Computer wins.")
+				} else {
+					fmt.Println("Player wins.")
+				}
+			default:
+				fmt.Println(playerChoice, "is not and option.")
+			}
+		}
+
+		// game round countdown, the the round cound down to zero - return (stop program)
+		round--
+		if round == 0 {
+			return
+		}
 	}
-	// fmt.Println("Player value is", playerValue)
 }
 
 // clearScreen clears the screen
